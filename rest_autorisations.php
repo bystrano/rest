fbdcaf4b88faccfb42ad350596dc36307ba1ef07
Michel Bystranowski <bystrano@gmx.ch>
2013-10-30 20:14:46 +0100

    supprime les commentaires et exemples de la fabrique

--- rest_autorisations.php
+++ rest_autorisations.php
@@ -11,12 +11,6 @@
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-
-/*
- * Un fichier d'autorisations permet de regrouper
- * les fonctions d'autorisations de votre plugin
- */
-
 /**
  * Fonction d'appel pour le pipeline
  * @pipeline autoriser */
@@ -27,19 +21,3 @@ function autoriser_rest_objet($verbe, $type, $id, $qui) {
 
   return in_array($qui['statut'], array('0minirezo', '1comite'));
 }
-
-/* Exemple
-function autoriser_configurer_rest_dist($faire, $type, $id, $qui, $opt) {
-	// type est un objet (la plupart du temps) ou une chose.
-	// autoriser('configurer', '_rest') => $type = 'rest'
-	// au choix
-	return autoriser('webmestre', $type, $id, $qui, $opt); // seulement les webmestres
-	return autoriser('configurer', '', $id, $qui, $opt); // seulement les administrateurs complets
-	return $qui['statut'] == '0minirezo'; // seulement les administrateurs (même les restreints)
-	// ...
-}
-*/
-
-
-
-?>
