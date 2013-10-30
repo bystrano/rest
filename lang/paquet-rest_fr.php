8c368d6b6727b79d30f35c5ef9795c634b8a568b
Michel Bystranowski <bystrano@gmx.ch>
2013-10-30 10:41:01 +0100

    commit initial - par la Fabrique

---
+++ lang/paquet-rest_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// R
+	'rest_description' => 'description ici…',
+	'rest_nom' => 'REST',
+	'rest_slogan' => 'Une API RESTful pour les objets éditoriaux',
+);
+
+?>
---
+++ rest_autorisations.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Définit les autorisations du plugin REST
+ *
+ * @plugin     REST
+ * @copyright  2013
+ * @author     Michel Bystranowski
+ * @licence    GNU/GPL
+ * @package    SPIP\Rest\Autorisations
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+/*
+ * Un fichier d'autorisations permet de regrouper
+ * les fonctions d'autorisations de votre plugin
+ */
+
+/**
+ * Fonction d'appel pour le pipeline
+ * @pipeline autoriser */
+function rest_autoriser(){}
+
+
+/* Exemple
+function autoriser_configurer_rest_dist($faire, $type, $id, $qui, $opt) {
+	// type est un objet (la plupart du temps) ou une chose.
+	// autoriser('configurer', '_rest') => $type = 'rest'
+	// au choix
+	return autoriser('webmestre', $type, $id, $qui, $opt); // seulement les webmestres
+	return autoriser('configurer', '', $id, $qui, $opt); // seulement les administrateurs complets
+	return $qui['statut'] == '0minirezo'; // seulement les administrateurs (même les restreints)
+	// ...
+}
+*/
+
+
+
+?>
